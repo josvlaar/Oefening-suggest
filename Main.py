14b0ced9f60a767df8cdb36ec1ffd65e95297af1
Jos Vlaar <github.com@josvlaar.com>
2022-09-30 15:58:27 +0200

    Changes

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
-with open('Opgaven.csv', 'r', encoding="utf-8") as file:
-    data = file.read()
+with open('Opgaven.csv', 'r', encoding="utf-8") as input:
+    data = input.read()
     print(data)
+
+result = ''
+# output moet string zijn, converteren met str(integer), met int(string) naar int
+
+with open('Result.csv', 'w', encoding="utf-8") as output:
+    output.write(result)
